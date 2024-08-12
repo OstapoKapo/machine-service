9d513c162158f510766f8455653ce06e42309728
Ostap Prodanyuk <OstapoKapo@gmail.com>
2024-08-13 02:16:34 +0300

    testing vercel

---
+++ src/app/api/user/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+import connect from '../../../utils/db'
+import User from '../../../models/User'
+
+export const GET = async (req) => {
+
+    try{
+        await connect();
+
+        const users = await User.find({});
+        return new NextResponse(JSON.stringify(users), {status: 200})
+    }catch(err){
+        return new NextResponse('bad', {status: 500})
+    }
+}
