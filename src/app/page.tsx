cdb78bdf8f06c64e0fbf1491f3d2d36017a54ca5
Ostap Prodanyuk <OstapoKapo@gmail.com>
2024-08-14 19:48:06 +0300

    add server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,13 @@ import axios from 'axios';
 
 export default function Home() {
 
-  const getApiUrl = () => {
-    if (process.env.NODE_ENV === 'development') {
-        return 'http://localhost:3000/api'; // Local development
-    } else {
-        // Use the VERCEL_URL environment variable for the production environment
-        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
-    }
-};
-
  useEffect( ()=>{
  const getUser = async () => {
-  const URL = getApiUrl()
     try {
-      await axios.get(`${URL}/user`, {})
+      await axios.post(`https://machine-service-server.onrender.com/api/user`, {})
       .then((response) => {
           if(response.status === 200 ){
-           console.log('dasd') 
+           console.log(response.data)
           }else{
             alert('somethin went wrong');
           }
