00b664eb1e6e1a225702269fbd4cad161aa066d5
Ostap Prodanyuk <OstapoKapo@gmail.com>
2024-08-15 18:58:56 +0300

    create style for btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,12 @@ export default function Home() {
   return (
     <div className="myContainer">
       <div className='myContainer__left'>
-          <Logo />
+        <div className='w-min h-full flex flex-col items-start justify-center'>
+        <Logo />
           <div className="btnGroup">
             <div className="btn btn_login">Log In</div>
           <div className="btn btn_register">Sign In</div>
+        </div>
         </div>
       </div>
       <div className='myContainer__right'>
